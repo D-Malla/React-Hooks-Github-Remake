c4402d91052bf558dcd101adac1008df19c0c94d
Dylan <dmalla@live.com>
2019-07-30 09:00:43 -0700

    Everything works except the Updated time

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import MaterialIcon, {colorPalette} from 'material-icons-react'
 import 'normalize.css/normalize.css'
 import '../styles/App.css'
@@ -13,10 +13,8 @@ export default function App(props) {
   const [email, setEmail] = useState('')
   const [image, setImage] = useState('')
   const [username, setUsername] = useState('')
-  const [repos, setRepos] = useState('')
+  const [repos, setRepos] = useState([])
   const [htmlUrl, setHtmlUrl] = useState('')
-
-  console.log(repos)
 
   useEffect(() => {
     axios.get('https://api.github.com/users/d-malla').then(resp => {
@@ -40,26 +38,26 @@ export default function App(props) {
       <header id='mainHeader'>
         <div>
           <img src={'https://place-hold.it/30'} alt='logo img'></img>
-          <input type='text'></input>
+          <input type='text' placeholder='Search or jump to...'></input>
           <ul>
-            <li>Pull requests</li>
-            <li>Issues</li>
-            <li>Marketplace</li>
-            <li>Explore</li>
+            <a href='#'><li>Pull requests</li></a>
+            <a href='#'><li>Issues</li></a>
+            <a href='#'><li>Marketplace</li></a>
+            <a href='#'><li>Explore</li></a>
           </ul>
         </div>
         <div id='icons'>
-          <div id='notificationsIcon' className='optionIcons'><MaterialIcon icon="notification_important" /></div>
-          <div id='repositoryIcon' className='optionIcons'><MaterialIcon icon="add" /></div>
-          <div id='userIcon' className='optionIcons'><img src={image} alt='tiny user'></img></div>
+          <a href='#'><div id='notificationsIcon' className='optionIcons'><MaterialIcon icon="notification_important" /></div></a>
+          <a href='#'><div id='repositoryIcon' className='optionIcons'><MaterialIcon icon="add" /><MaterialIcon icon="arrow_drop_down" id='firstArrow'/></div></a>
+          <a href='#'><div id='userIcon' className='optionIcons'><img src={image} alt='tiny user'></img><MaterialIcon icon="arrow_drop_down" id='secondArrow' /></div></a>
         </div>
       </header>
       <section>
         <aside>
-          <div>
+          <div id='asideImage'>
             <img src={image} alt='user'></img>
-            <h1>{username}</h1>
-            <h3>{name}</h3>
+            <h1>{name}</h1>
+            <h3>{username}</h3>
             <button>Follow</button>
           </div>
           <div>
@@ -67,19 +65,19 @@ export default function App(props) {
             <p><MaterialIcon icon="star" /></p>
             <p id='pro'>PRO</p>
             </div>
-            <p>Block or report user</p>
+            <a href='#'><p id='blockReport'>Block or report user</p></a>
           </div>
 
         </aside>
         <main>
           <div id="bodyMenu">
             <ul>
-              <li className='menuItems'>Overview</li>
-              <li className='menuItems'>Repositories</li>
-              <li className='menuItems'>Projects</li>
-              <li className='menuItems'>Stars</li>
-              <li className='menuItems'>Followers</li>
-              <li className='menuItems'>Following</li>
+              <a href='#'><li className='menuItems'>Overview</li></a>
+              <a href='#'><li className='menuItems'>Repositories</li></a>
+              <a href='#'><li className='menuItems'>Projects</li></a>
+              <a href='#'><li className='menuItems'>Stars</li></a>
+              <a href='#'><li className='menuItems'>Followers</li></a>
+              <a href='#'><li className='menuItems'>Following</li></a>
             </ul>
           </div>
           <div id='searchMenu'>
@@ -104,33 +102,33 @@ export default function App(props) {
               <option>PHP</option>
             </select>
           </div>
-          <div id='repositoryList'>
-          {/* {repos.map(repo => ( */}
+          {repos.map(repo => (
+          <div key={'album-id' + repo.id}id='repositoryList'>
             <div>
               <div>
-                <h3>foobar</h3>
+                <a href='#'><h3>{repo.name}</h3></a>
                 <div id='aboutRepo'>
                   <p>HTML</p>
-                  <p>Updated -- days ago</p>
+          <p>Updated -- days ago</p>
                 </div>
               </div>
               <div>
-                <button type='button'>Star</button>
+                <button type='button'>★ Star</button>
               </div>
             </div>
-          {/* ))} */}
           </div>
+          ))}
         </main>
       </section>
       <footer id='mainFooter'>
         <div>
           <ul>
             <li>© 2019 Github, Inc.</li>
-            <li>Terms</li>
-            <li>Privacy</li>
-            <li>Security</li>
-            <li>Status</li>
-            <li>Help</li>
+            <a href='#' className='bottomLink'><li>Terms</li></a>
+            <a href='#' className='bottomLink'><li>Privacy</li></a>
+            <a href='#' className='bottomLink'><li>Security</li></a>
+            <a href='#' className='bottomLink'><li>Status</li></a>
+            <a href='#' className='bottomLink'><li>Help</li></a>
           </ul>
         </div>
         <div>
@@ -138,12 +136,12 @@ export default function App(props) {
         </div>
         <div>
           <ul>
-            <li>Contact Github</li>
-            <li>Pricing</li>
-            <li>API</li>
-            <li>Training</li>
-            <li>Blog</li>
-            <li>About</li>
+            <a href='#' className='bottomLink'><li>Contact Github</li></a>
+            <a href='#' className='bottomLink'><li>Pricing</li></a>
+            <a href='#' className='bottomLink'><li>API</li></a>
+            <a href='#' className='bottomLink'><li>Training</li></a>
+            <a href='#' className='bottomLink'><li>Blog</li></a>
+            <a href='#' className='bottomLink'><li>About</li></a>
           </ul>
         </div>
       </footer>
